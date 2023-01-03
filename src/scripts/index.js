905f1bf20b64ea0ae9011258f41d9d909f8555c4
Victor Parizio <victorparizio@hotmail.com>
2023-01-03 15:22:24 -0300

    validação empty (alertar que o campo está vazio e não fazer busca) para campo vazio

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,6 +5,7 @@ import { screen } from '/src/scripts/objects/screen.js'
 
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
+    if(validdateEmptyInput(userName)) return
     getUserData(userName)
 })
 
@@ -12,14 +13,22 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
     const userName = e.target.value
     const key = e.which || e.keyCode
     const isEnterKeyPresed = key === 13
-
+    
     if (isEnterKeyPresed) {
+        if(validdateEmptyInput(userName)) return
         getUserData(userName)
-    }
+    }    
 })
 
-async function getUserData(userName) {
+function validdateEmptyInput(userName){
+    if(userName.length === 0){
+        alert('Preencha o campo com o usuário do GitHub')
+        return true
+    }
+}
 
+async function getUserData(userName) {
+    
     const userResponse = await getUser(userName)
     const repositoriesResponse = await getRepositories(userName)
     user.setInfo(userResponse)
