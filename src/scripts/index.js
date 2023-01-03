c9c7700ddaa1faa4ea7f88223de0cb8e916e03da
Victor Parizio <victorparizio@hotmail.com>
2023-01-03 10:57:06 -0300

    refatoração de url em variáveis

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+import { baseUrl, repositoriesQuantity } from '/src/scripts/variable.js'
+
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
     getUserProfile(userName)
@@ -14,12 +16,12 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
 })
 
 async function user(userName) {
-    const response = await fetch(`https://api.github.com/users/${userName}`)
+    const response = await fetch(`${baseUrl}/${userName}`)
     return await response.json()
 }
 
 async function repos(userName) {
-    const response = await fetch(`https://api.github.com/users/${userName}/repos`)
+    const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoriesQuantity}`)
     return await response.json()
 }
 
